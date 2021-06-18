cd3d3ecc8ed1a36e2a84a2d9064d26342b3839a2
Ameya Deokule <deokuleameya@gmail.com>
2021-06-18 13:52:01 +0530

    added basic styles, create file structure and created homePage slice

---
+++ src/app/container/HomePage/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface IHomePageProps{
+	
+}
+
+const Container = styled.div`
+	width:100%;
+	height:100%;
+	display:flex;
+	flex-direction:column;
+	align-items:center;
+`
+
+
+
+const HomePage = (props: IHomePageProps) => {
+	return (
+		<Container>
+			<h1>Hot Anime List</h1>
+		</Container>
+	)
+}
+
+export default HomePage
